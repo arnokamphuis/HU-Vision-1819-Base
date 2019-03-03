8a52e6dae57634b55c6de06a3d8877c039ce4a08
gerritvanos <gerrit.vanos@student.hu.nl>
2019-03-03 11:47:24 +0100

    added grayscale

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,8 +1,16 @@
 #include "StudentPreProcessing.h"
+#include "ImageFactory.h"
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	return nullptr;
+	IntensityImage * output = ImageFactory::newIntensityImage(image.getWidth(),image.getHeight());
+	int size = image.getHeight() * image.getWidth();
+	for (int i = 0;i < size;i++) {
+		auto RGBPix = image.getPixel(i);
+		int newIntensity = static_cast<int>(0.3*RGBPix.r + 0.59*RGBPix.g + 0.11*RGBPix.b);
+		output->setPixel(i, newIntensity);
+	}
+	return output;
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
